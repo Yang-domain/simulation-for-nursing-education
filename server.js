c8caeb85e4cd9da2a754a195bf2afd4c273e4c9e
Yang-domain <yangjinuk0912@gmail.com>
2025-08-21 14:08:34 +0900

    14시08분 수정

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ import path from "path";
 import { fileURLToPath } from "url";
 
 dotenv.config();
+console.log("SCENARIO_PROMPT:", process.env.SCENARIO_PROMPT);
 
 const app = express();
 app.use(cors());
